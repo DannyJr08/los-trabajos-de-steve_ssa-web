053f6d7b6fad279a4307ce58b1cf940527969b4d
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-09-18 17:55:17 -0500

    Instalación de Dependencias
    React Router Dom
    Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Login from './components/pages/Login/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element={<Navigate to="/login" replace={true} />}/>    {/* la página default será Login, Navigate sirve para redireccionar */}
+          <Route path='/login' element={<Login />} />
+          
+          {/* Agregar aqui la ruta a los componentes que se vayan importando.   /inicio es como aparecerá en el URL de la página */}
+        </Routes>
+        {/* <footer>
+          <MenuFooter />
+          <Footer />
+        </footer> */}
+      </BrowserRouter>
     </div>
   );
 }
