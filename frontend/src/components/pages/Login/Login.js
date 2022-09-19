81e45abd530ce388645b0a99e21e2af888a7106a
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-09-18 19:37:02 -0500

    Login y Arreglo de Bootstrap y Sass

--- frontend/src/components/pages/Login/Login.js
+++ frontend/src/components/pages/Login/Login.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
+// Importación de imágenes jpg, png y svg
+
+// Importación de estilos
+import './Login.css'
+
 function Login(){
-    const form = document.querySelector("form")
+    // const form = document.querySelector("form")
 
-        form.addEventListener('submit', e => {
-            if (!form.checkValidity()) {
-                e.preventDefault()
-            }
+    //     form.addEventListener('submit', e => {
+    //         if (!form.checkValidity()) {
+    //             e.preventDefault()
+    //         }
 
-            form.classNameList.add('was-validated')
-        })
+    //         form.classNameList.add('was-validated')
+    //     })
     return (
         <div className="container w-80 bg-primary mt-3 mb-3 rounded shadow">
         <div className="row align-items-stretch">
-            <div className="col rounded-start p-5" style="background-color: #C6D0F2;">
+            <div className="col rounded-start p-5" style={{backgroundColor: '#C6D0F2'}}>
                 <div className="text-center mb-5">
                     <h3 className="page-title">Bienvenido a</h3>
                     <h1 className="page-title">Salud - OS</h1>
@@ -23,13 +28,13 @@ function Login(){
                 <form novalidate>
                     <div className="form-floating mb-3">
                         <input type="email" id="email" className="form-control" placeholder="Escriba su correo electrónico" required></input>
-                        <label for="email" className="form-label text-form" style="opacity: 0.5">Correo Electrónico</label>
+                        <label for="email" className="form-label text-form" style={{opacity: 0.5}}>Correo Electrónico</label>
                         <div className="invalid-feedback">Correo inválido</div>
                         <div className="valid-feedback">Correo válido</div>
                     </div>
                     <div className="form-floating mb-0">
                         <input type="password" id="email" className="form-control" placeholder="Escriba su contraseña" required></input>
-                        <label for="email" className="form-label text-form" style="opacity: 0.5">Contraseña</label>
+                        <label for="email" className="form-label text-form" style={{opacity: 0.5}}>Contraseña</label>
                         <div className="invalid-feedback">Contraseña Inválida</div>
                         <div className="valid-feedback">Contraseña válida</div>
                     </div>
